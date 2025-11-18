944263894a9d183cca27093b2be3f20ec6776ff8
Joshd667 <joshduddy@outlook.com>
2025-11-18 22:36:51 +0000

    clear repo

--- js/features/mindmaps/filter.js
+++
@@ -1,8 +0,0 @@
-// js/features/mindmaps/filter.js
-// Filtering logic for mindmaps view
-// Now uses shared contentFilter utility to eliminate code duplication
-
-import { createContentFilterMethods } from '../../utils/content-filter.js';
-
-// Export filter methods for mindmaps (generated from shared utility)
-export const mindmapsFilterMethods = createContentFilterMethods('mindmaps');
